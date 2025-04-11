4129144e251c74281d6ea74bf8ee2003ab6dd56d
trey153 <trey153@headmind.com>
2025-04-11 15:41:49 +0200

    Update main.tf
    tfstate bucket name changed

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3412,7 +3412,7 @@ resource "aws_s3_object" "upload_temp_object_2" {
 }
 /* Creating a S3 Bucket for Terraform state file upload. */
 resource "aws_s3_bucket" "bucket_tf_files" {
-  bucket        = "do-not-delete-awsgoat-state-files-${data.aws_caller_identity.current.account_id}"
+  bucket        = "do-not-delete-awsgoat-state-files-101010"
   force_destroy = true
   tags = {
     Name        = "Do not delete Bucket"
